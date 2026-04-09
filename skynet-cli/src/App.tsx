f99c23a08ce444c70d1ef53945f205fe6e7f1fa3
debnix <daquipe5@gmail.com>
2026-04-08 20:07:05 -0500

    refactor: add terminal

--- skynet-cli/src/App.tsx
+++ skynet-cli/src/App.tsx
@@ -1,34 +1,56 @@
 import './App.css'
 import { ReactTerminal } from "react-terminal";
+import { HeaderCLI } from './HeaderCLI';
+import Terminal, { ColorMode, TerminalOutput } from 'react-terminal-ui';
+import { useState } from 'react';
+
+const API_URL = 'http://localhost:8000/entity';
 
 function App() {
- // Define commands here
- const commands = {
-  whoami: "debnix",
-  cd: (directory) => `changed path to ${directory}`
-};
+  const [terminalLineData, setTerminalLineData] = useState([
+    <TerminalOutput>Welcome to the React Terminal UI Demo!</TerminalOutput>,
+  ]);
 
-return (
-  <div style={{
-    width: '100%',
-    height: '50vh'
-  }}> 
-  <ReactTerminal
-  showControlBar={false}
-    commands={commands}
-    theme='matrix'
-    style={{
-      width: '100%',
-      height: '50vh'
-    }}
-    width='100vw'
-    height='90%'
-    defaultHandler={(command) => {
-      console.log(command);
-    }}
-  />
-  </div>
-);
+  const fetchMessage = async (text: string) => {
+    try {
+      const response = await fetch(API_URL, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ prompt: text }),
+      });
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error('Error fetching message:', error);
+      return 'Error fetching message';
+    }
+  };
+
+  const defaultHandler = async(command: string) => {
+    const response = await fetchMessage(command);
+    setTerminalLineData([...terminalLineData, <TerminalOutput> {'>>>'}{response}</TerminalOutput>]);
+  };
+
+  return (
+    <div className='app-container'>
+
+      <div className='title-container'>
+        <h2 className='title'>Terminator AI</h2>
+      </div>
+
+      <div className='terminal-container'> 
+      <Terminal
+        name="React Terminal Usage Example"
+        colorMode={ColorMode.Light}
+        onInput={defaultHandler}
+      >
+        {terminalLineData}
+      </Terminal>
+      </div>
+    </div>
+  );
 }
 
 export default App
---
+++ skynet-cli/src/HeaderCLI.tsx
@@ -0,0 +1,9 @@
+export function HeaderCLI() {
+  return (
+    <div style={{textAlign: 'center'}}>
+      <h1>Welcome to Skynet CLI</h1>
+      <h2>🔴</h2>
+      <h2>Talk to me 🤖</h2>
+    </div>
+  );
+}
